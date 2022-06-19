67feb7b1411d7cfd0371af8c3a13bd094500e02e
vishal <vishalkajales@gmail.com>
2022-06-20 01:42:58 +0530

    Starting of flutter bloc practice by vandad

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Flutter demo",
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: const MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MyHomePage"),
+      ),
+    );
+  }
+}
